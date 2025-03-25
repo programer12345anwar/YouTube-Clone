8abca06a332e9a851ee55c6cc860abe1140a29b2
programer12345anwar <mdanwar40212@gmail.com>
2025-03-25 23:12:35 +0530

    update in configuration

--- youtube-backend/central/src/main/java/com/youtube/central/configuration/RabbitMQConfig.java
+++ youtube-backend/central/src/main/java/com/youtube/central/configuration/RabbitMQConfig.java
@@ -1,5 +1,10 @@
 package com.youtube.central.configuration;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -8,33 +13,48 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+    //configuration for RabbitMQ
+
+    public final static String exchangeName = "notification-exchange";
+    public final static String queueName = "notification-queue";
+    public final static String routingKey = "notification-123";
+
+    @Bean
+    public DirectExchange getDirectExchange() {
+        return new DirectExchange(exchangeName);
+    }
+
+    @Bean
+    public Queue getMessagingQueue() {
+        return QueueBuilder.durable(queueName).build();  // Fixed issue: using queueName instead of exchangeName
+    }
 
     @Bean
     public CachingConnectionFactory getConnectionFactory() { 
-        //CachingConnection factory is nothing but a connection to rabbitmq
+        // CachingConnectionFactory establishes a connection to RabbitMQ
         CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
         connectionFactory.setUsername("guest");
         connectionFactory.setPassword("guest");
         return connectionFactory;
     }
+
     @Bean
     public RabbitTemplate getRabbitTemplate(CachingConnectionFactory connectionFactory) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
         return rabbitTemplate;
-        
     }
-   
-    // public static final String queueName = "notification-queue";
-    // public static final String exchangeName = "notification-exchange";
-    // public static final String routingKey = "notification-123";
 
-    // @Bean
-    // public AMQP.Queue queue() {
-    //     return new AMQP.Queue();
-    // }
+    public Binding bindingQueueWithExchange(DirectExchange exchange, Queue queue) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
 }
+
+   
+    
+
  /*
+    NOTES:
      * for explaining @Bean
      * @Autowired
      * RestTemplate restTemplate;//spring boot will not able to provide the object for the restTemplate b'z RestTemplate doesn't have @Component,@Service, @Repository, @Controller,so you need to configure it.
