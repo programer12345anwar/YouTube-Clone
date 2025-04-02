99a4c76bdac5bf458bca9b6f3567aa38d5dd1e63
programer12345anwar <mdanwar40212@gmail.com>
2025-04-03 00:08:30 +0530

    notification sending

--- youtube-backend/notification-api/notification-api/src/main/java/com/youtube/notification_api/controller/CommonController.java
+++ youtube-backend/notification-api/notification-api/src/main/java/com/youtube/notification_api/controller/CommonController.java
@@ -1,6 +1,7 @@
 package com.youtube.notification_api.controller;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.youtube.notification_api.dto.NotificationMessage;
@@ -12,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 public class CommonController {
 
     @RabbitListener(queues="notification-queue")
-    public void consumeMessage(NotificationMessage message) {
+    public void consumeMessage(@Payload NotificationMessage message) {
         log.info(message.toString());
     }
     
