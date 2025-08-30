366547cf08e11650ca51fc1dfd85857c38b53ed1
Md Anwar Alam <mdanwar40212@gmail.com>
2025-08-31 01:14:45 +0530

    hidden secret keys in central-service by creating .env file

--- youtube-backend/central/src/main/java/com/youtube/central/configuration/RabbitMQConfig.java
+++ youtube-backend/central/src/main/java/com/youtube/central/configuration/RabbitMQConfig.java
@@ -9,10 +9,59 @@ import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    @Bean
+    public DirectExchange getDirectExchange() {
+        return new DirectExchange(exchangeName);
+    }
+
+    @Bean
+    public Queue getMessagingQueue() {
+        return QueueBuilder.durable(queueName).build();
+    }
+
+    @Bean
+    public CachingConnectionFactory getConnectionFactory(
+            @Value("${spring.rabbitmq.host}") String host,
+            @Value("${spring.rabbitmq.username}") String username,
+            @Value("${spring.rabbitmq.password}") String password) {
+
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        return connectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate getRabbitTemplate(CachingConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Binding bindQueueWithExchange(DirectExchange exchange, Queue queue) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+}
+
+/*
 @Configuration
 public class RabbitMQConfig {
 
@@ -54,6 +103,8 @@ public class RabbitMQConfig {
         return BindingBuilder.bind(queue).to(exchange).with(routingKey);
     }
 }
+*/
+
 
    
     
